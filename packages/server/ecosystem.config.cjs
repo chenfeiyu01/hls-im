01b30859412226a30d235f26f1583323a37f7d26
chenfeiyu01 <849767414@qq.com>
2024-12-24 15:36:29 +0800

    feat: 增加部署脚本

--- packages/server/ecosystem.config.cjs
+++ packages/server/ecosystem.config.cjs
@@ -1,10 +1,12 @@
 module.exports = {
   apps: [{
     name: 'chat-server',
-    script: 'dist/index.js',
+    script: './dist/index.js',
     env: {
       PORT: 8090,
       NODE_ENV: 'production'
-    }
+    },
+    exec_mode: 'cluster',
+    instances: 1
   }]
 } 
